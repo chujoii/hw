788b5218dba68d774990c58edafdf7021d298b25
Roman V. Prikhodchenko <chujoii@gmail.com>
2020-05-09 00:13:08 +0300

    Make raster for sauce-boat hat with two color

---
+++ share/hedgewars/Data/Graphics/Hats/sauce-boat/make-hat.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+../template/rasterisation-hat.sh 'sauce-boat' '--export-id-only --export-id=layer2' 'dish_SauceBoatSilver'
+../template/rasterisation-hat.sh 'sauce-boat' '--export-id-only --export-id=layer4' 'dish_SauceBoatGold'
