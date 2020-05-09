e16aef034b62d9badb10be20f9a27c1cab5bfe6f
Roman V. Prikhodchenko <chujoii@gmail.com>
2020-05-09 20:49:04 +0300

    Make raster script for rag-doll

---
+++ share/hedgewars/Data/Graphics/Hats/rag-doll/make-hat.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+../template/rasterisation-hat.sh 'rag-doll' '' 'rag-doll-2'
+
+# see ../brain-slug/make-hat.sh
+cp rag-doll.svg tmp.svg
+# show 4 holes flat (sew-through) buttons eyes
+perl -0777 -i -pe 's/(inkscape:label="hat4"\n[^"]*)style="display:none"/$1style="display:inline"/igs' tmp.svg
+# hide 2 holes flat (sew-through) buttons eyes
+perl -0777 -i -pe 's/(inkscape:label="hat2"\n[^"]*)style="display:inline"/$1style="display:none"/igs' tmp.svg
+../template/rasterisation-hat.sh 'tmp' '' 'rag-doll-4'
