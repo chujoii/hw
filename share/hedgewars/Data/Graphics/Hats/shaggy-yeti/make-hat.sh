72419d035dfd702fd3b9c353bcb24e33680dd1ef
Roman V. Prikhodchenko <chujoii@gmail.com>
2020-05-10 13:54:09 +0300

    Make raster script for shaggy-yeti and chicken

---
+++ share/hedgewars/Data/Graphics/Hats/shaggy-yeti/make-hat.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+../template/rasterisation-hat.sh 'shaggy-yeti' '' 'ShaggyYeti'
+
+# see ../brain-slug/make-hat.sh
+cp shaggy-yeti.svg tmp.svg
+# show beak
+perl -0777 -i -pe 's/(inkscape:label="beak"\n[^"]*)style="display:none"/$1style="display:inline"/igs' tmp.svg
+# yellow filter
+perl -0777 -i -pe 's/(inkscape:label="yellow filter\. Layers: Blend mode: Multiply \(enable-background\)"\n[^"]*)style="display:none/$1style="display:inline/igs' tmp.svg
+# hide mouth
+perl -0777 -i -pe 's/(inkscape:label="mouth"\n[^"]*)style="display:inline"/$1style="display:none"/igs' tmp.svg
+../template/rasterisation-hat.sh 'tmp' '' 'zoo_chicken'
